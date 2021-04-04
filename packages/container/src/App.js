6ea5ab8891e0f64c2ba685f0654a42d6aaea10e6
kumar indala <91934@DESKTOP-VV8OPAL.micelio.local>
2021-04-04 18:15:09 +0530

    first commit

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,6 @@ import Footer from "./Layout/Footer";
 import Header from "./Layout/Header";
 import Home from "./Screens/Home/Home";
 import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
-import About from "./Screens/About/About";
 
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
                 <Redirect to="/home" />
             </Route>
  <Route exact path='/home' component={Home} />
- <Route exact path='/about' component={About} />
 
 </Switch>
 <Footer />
